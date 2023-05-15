b5617318f35a3b325654f08fd5cbcf78844f1c93
Martin Maťovčík <116836668+martinmatovcik@users.noreply.github.com>
2023-05-15 19:49:45 +0200

    Initial commit of Tribes project

---
+++ src/test/java/com/mmatovcik/tribes/TribesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mmatovcik.tribes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribesApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
