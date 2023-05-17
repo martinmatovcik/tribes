3fca30f21937604100879565edc9113c800a7412
Martin Maťovčík <116836668+martinmatovcik@users.noreply.github.com>
2023-05-17 17:51:43 +0200

    feat: Kingdom model updated

--- src/main/java/com/mmatovcik/tribes/models/Kingdom.java
+++ src/main/java/com/mmatovcik/tribes/models/Kingdom.java
@@ -1,16 +1,26 @@
 package com.mmatovcik.tribes.models;
 
+import java.util.LinkedList;
+import java.util.List;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "kingdoms")
+@NoArgsConstructor
 public class Kingdom {
   @Id private String id;
   private TribesUser user;
+  private String name;
+  private List<Troop> troops = new LinkedList<>();
+  private List<Resource> resources = new LinkedList<>();
+  private Location location;
 
-  public Kingdom(TribesUser user) {
+  public Kingdom(TribesUser user, String name, Location location) {
     this.user = user;
+    this.name = name;
+    this.location = location;
   }
 }
