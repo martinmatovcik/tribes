f2f6a84c1d1957f0fade1771474bf8c4eb4f0434
Martin Maťovčík <116836668+martinmatovcik@users.noreply.github.com>
2023-05-19 19:17:46 +0200

    feat: when register first user -> create admin in db, logic for login

--- src/main/java/com/mmatovcik/tribes/services/TribesUserServiceImpl.java
+++ src/main/java/com/mmatovcik/tribes/services/TribesUserServiceImpl.java
@@ -2,10 +2,19 @@ package com.mmatovcik.tribes.services;
 
 import com.mmatovcik.tribes.exceptions.NotFoundException;
 import com.mmatovcik.tribes.exceptions.NotUniqueException;
+import com.mmatovcik.tribes.models.Role;
 import com.mmatovcik.tribes.models.TribesUser;
 import com.mmatovcik.tribes.repositories.TribesUserRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +22,16 @@ import org.springframework.stereotype.Service;
 public class TribesUserServiceImpl implements TribesUserService {
   private final TribesUserRepository userRepository;
   private final KingdomService kingdomService;
+  private final AuthenticationManager authenticationManager;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
 
   @Override
   public TribesUser register(TribesUser userToRegister, String kingdomName) {
+    if (isUserDatabaseEmpty()) {
+      addAdminToDatabase();
+    }
+
     String usernameToRegister = userToRegister.getUsername();
 
     if (usernameExists(usernameToRegister)) {
@@ -23,10 +39,21 @@ public class TribesUserServiceImpl implements TribesUserService {
     }
 
     TribesUser registeredUser =
-        userRepository.save(
-            new TribesUser(usernameToRegister, userToRegister.getPassword()));
+        new TribesUser(
+            usernameToRegister, passwordEncoder.encode(userToRegister.getPassword()), Role.USER);
+
+    userRepository.save(registeredUser);
     kingdomService.createKingdom(registeredUser, kingdomName);
+
     return registeredUser;
+  }
+
+  private void addAdminToDatabase() {
+    userRepository.save(new TribesUser("admin", passwordEncoder.encode("admin"), Role.ADMIN));
+  }
+
+  private Boolean isUserDatabaseEmpty() {
+    return userRepository.findAll().isEmpty();
   }
 
   private Boolean usernameExists(String username) {
@@ -41,10 +68,29 @@ public class TribesUserServiceImpl implements TribesUserService {
 
   @Override
   public String login(String username, String password) {
-    return "JWT token";
+    try {
+      authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(username, password));
+    } catch (AuthenticationException e) {
+      throw new BadCredentialsException("Username or password is not valid.");
+    }
+    TribesUser user = findUserByUsername(username);
+
+    return jwtService.generateToken(user);
   }
 
   private Optional<TribesUser> loadOptionalOfUserByUsername(String username) {
     return userRepository.findByUsername(username);
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+      throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
